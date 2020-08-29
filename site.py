11b4fd166938e9f8b53b86f4bafdc6e63ce7060b
Gasoline-alt <70410141+Gasoline-alt@users.noreply.github.com>
2020-08-29 17:12:40 -0500

    Update site.py

--- site.py
+++ site.py
@@ -3,12 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('https://gasoline-alt.github.io/Gasosite/')
 def index():
 	return render_template("index.html")
 
 
-@app.route('/schedule')
+@app.route('https://gasoline-alt.github.io/Gasosite/schedule')
 def about():
 	return render_template("schedule.html")
 
